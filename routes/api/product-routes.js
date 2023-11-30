a9b6b3a6e97fb118211810f5351b70ac3e64e009
Nomin Myagmarsuren <nominmyagmarsuren@gmail.com>
2023-11-30 00:26:14 -0700

    finish routes, fix product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -133,6 +133,17 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
 });
 
 module.exports = router;
